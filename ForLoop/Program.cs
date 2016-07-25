b1de4d1cf1e64460c7f30ea3bfbde2e8d7e08be8
Zachary Toney <Zachary Toney>
2016-07-25 11:27:19 -0400

    For Loop

---
+++ ForLoop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int a = 3; a <= 5; a++)
+            {
+                Console.WriteLine(a);
+            }
+            Console.ReadLine();
+        }
+    }
+}
