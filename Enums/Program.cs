0ea8868e148d152efa59da23ba41ddb3f8de4edf
Zachary Toney <Zachary Toney>
2016-07-25 11:01:52 -0400

    Enums

---
+++ Enums/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    class Program
+    {
+        
+        enum People { Tim,Jim };
+
+        static void Main()
+        {
+            int x = (int)People.Tim;
+            int y = (int)People.Jim;
+            Console.WriteLine("Tim", x);
+            Console.WriteLine("Jim", y);
+            Console.ReadLine();
+        }
+    }
+}
