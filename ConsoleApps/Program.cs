02dade75966cadf8666f4642ef5b5defe441119f
Zachary Toney <Zachary Toney>
2016-07-25 09:39:45 -0400

    Initial Commit

--- ConsoleApps/Program.cs
+++ ConsoleApps/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApps
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello Console");
+            Console.ReadLine();
         }
     }
 }
