0da9c6baa5040c018435d5e10b5eb3f6064934f7
Zachary Toney <Zachary Toney>
2016-07-25 10:37:52 -0400

    Showing how Collection works

---
+++ Collection/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ProgrammingLanguages = new List<string>();
+            ProgrammingLanguages.Add("R");
+            ProgrammingLanguages.Add("Python");
+            ProgrammingLanguages.Add("Ruby");
+            ProgrammingLanguages.Add("C#");
+            ProgrammingLanguages.Add("PHP");
+
+            foreach (var Language in ProgrammingLanguages)
+            {
+                Console.WriteLine(Language + " ");
+            }
+            Console.ReadLine();
+            //The output will be all the languages
+        }
+    }
+}
