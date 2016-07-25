d6f89cc26756c748a08ac1a011668cb582aee1a4
Zachary Toney <Zachary Toney>
2016-07-25 10:16:27 -0400

    Built-In Types

---
+++ BuiltInTypes/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuiltInTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool a = true;
+            byte b = 255;
+            sbyte c = -128;
+            char d = (char)88;
+            decimal e = 42.2m;
+            double f = 3D;
+            float g = 5F;
+            int h = 10;
+            uint i = 123;
+            long j = 4294967296;
+            ulong k = 9223372036854775808;
+            object l = 1;
+            short m = 32767;
+            ushort n = 65535;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            Console.WriteLine(g);
+            Console.WriteLine(h);
+            Console.WriteLine(i);
+            Console.WriteLine(j);
+            Console.WriteLine(k);
+            Console.WriteLine(l);
+            Console.WriteLine(m);
+            Console.WriteLine(n);
+        }
+    }
+}
