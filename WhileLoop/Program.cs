e472de748d6fafecc95d7910a2e2ee6d2e1fb4b8
Zachary Toney <Zachary Toney>
2016-07-25 11:25:03 -0400

    While Loop

---
+++ WhileLoop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            while(a <= 10)
+            {
+                Console.WriteLine(a);
+                a++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
