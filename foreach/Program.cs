f3a5858e62da529477050141c9ca19e77165a3a9
Zachary Toney <Zachary Toney>
2016-07-25 11:13:10 -0400

    foreach

---
+++ foreach/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers;
+            numbers = new int[] {1,2,3,4,5,6,7,8,9};
+            foreach(int number in numbers)
+            {
+                Console.WriteLine(number + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
